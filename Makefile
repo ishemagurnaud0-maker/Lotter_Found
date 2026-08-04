391849ae2b5a3cc71264fc62fe6bfdeee83fde70
Gurnaud <ishemagurnaud0@gmail.com>
2026-08-04 14:46:30 +0100

    [refactor]:Added the VRFV2PlusClient to request RandomWords.

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 -include .env
 
 build:; forge build
-anvil_deploy:; forge script script/Counter.s.sol:CounterScript --rpc-url http://127.0.0.1:8545 --account deployer --broadcast -vvvv
+anvil_deploy:; forge script script/Counter.s.sol:CounterScript --rpc-url http://127.0.0.1:8545 --private-key -- --broadcast -vvvv
+sepolia_deploy:; forge script script/Counter.s.sol:CounterScript --rpc-url $(SEPOLIA_RPC_URL) --account deployer --broadcast -vvvv
 test:; forge test --match-contract CounterTest 
 snap:; forge snapshot
--- src/Raffle.sol
+++ src/Raffle.sol
@@ -31,6 +31,7 @@ import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/V
 
 error Raffle__NotEnoughFunds();
 error Raffle__NotTheWinner();
+error Raffle__SetTimeHasNotElapsed();
 
 
 /**
@@ -41,20 +42,25 @@ error Raffle__NotTheWinner();
  */
 
 
- contract Raffle {
-    
+ contract Raffle is VRFConsumerBaseV2Plus,VRFV2PlusClient{
+
+    uint256 private immutable i_lotteryTimeInterval;
     uint256 private immutable i_entranceFee;
-    address private immutable i_vrfCoordinator;
+   // address private immutable i_vrfCoordinator;
     address payable[] s_players;
+    uint256 private s_lastTimeStamp;
+    uint256 private s_subId;
 
     event PlayerAddedToRaffle(address indexed player);
 
-    constructor(uint256 entranceFee,address vrfCoordinator){
-        i_entranceFee = entranceFee;
-        i_vrfCoordinator = vrfCoordinator;
+    constructor(uint256 _entranceFee,uint256 _subId,uint256 _timeInterval)VRFConsumerBaseV2Plus(vrfCoordinator){
+        i_entranceFee = _entranceFee;
+        i_lotteryTimeInterval = _timeInterval;
+        s_lastTimeStamp = block.timestamp;
+        s_subId = _subId;
     }
 
-    function enterRaffle() public payable{
+    function enterRaffle() external payable{
         if(msg.value < i_entranceFee){
             revert Raffle__NotEnoughFunds();
         }
@@ -63,8 +69,22 @@ error Raffle__NotTheWinner();
         emit PlayerAddedToRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if(block.timestamp - s_lastTimeStamp < i_lotteryTimeInterval) revert Raffle__SetTimeHasNotElapsed();
 
+        VRFV2PlusClient.requestRandomWords request = VRFV2PlusClient.requestRandomWords({
+            keyHash:s_keyHash,
+            subId:s_subscriptionId,
+            requestConfirmations: requestConfirmations,
+            callbackGasLimit: callbackGasLimit,
+            numWords: numWords,
+            extraArgs: VRFV2PlusClient._argsToBytes(VRFV2PlusClient.ExtraArgsV1({nativePayment: false}))
+        });
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual{}
+
+111156905340625053542389350583940108777420172393988756622693613794872779825267 // subId
 
     /*Getter functions*/
     function getEntranceFee() external view returns(uint256) {
